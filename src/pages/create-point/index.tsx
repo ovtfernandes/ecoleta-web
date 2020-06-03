47ced8d4ab4bd2bf086be3d41a40089a9b43ad4c
Vitor Fernandes <s.fernandes.vitor@gmail.com>
2020-06-03 14:00:59 -0300

    Adds map visualization to create-point page

--- src/pages/create-point/index.tsx
+++ src/pages/create-point/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
+import { Map, TileLayer, Marker } from 'react-leaflet';
 
 import './styles.css';
 
@@ -61,6 +62,16 @@ const CreatePoint = () => {
                         <h2>Endereço</h2>
                         <span>Selecione o endereço no mapa</span>
                     </legend>
+
+                    <Map center={{lat: 51.505, lng: -0.09}} zoom={15}>
+                        <TileLayer
+                            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        />
+
+                        <Marker position={{lat: 51.505, lng: -0.09}} />
+                    
+                    </Map>
 
                     <div className="field-group">
                         <div className="field">
